3ab7339ebc05030ed40c09d19d967b188a7291b1
Kevin Sage <ksagedeveloper@gmail.com>
2019-11-17 23:11:38 +0000

    Done.

--- fix_using_tests/pemdos.rb
+++ fix_using_tests/pemdos.rb
@@ -1,6 +1,11 @@
 def snake_it_up(string)
   if string[0] == "s"
-  10 * "s" + string
+  #10 * "s" + string
+  repeat = 10
+  hiss = 's' * repeat
+  hiss.concat(string)
+#binding.pry
+
   else
   string
   end
